46dc2aa7100c8e473800caf7d54cc12ee2c9d8c1
kevinngonzales <kevinngstem@gmail.com>
2023-11-03 12:30:08 -0700

    added files

---
+++ script.js
@@ -0,0 +1,14 @@
+// Create an HTML and JS file and link them with the script tag
+// Create at least 3 different elements in the body of your html file
+// In your JS file select each element and log them to the console 
+// Select multiple elements with querySelectorAll() and log them to the console
+
+let heading = document.querySelector("h1"); 
+
+let paragraph = document.getElementById("paragraph");
+
+let listItems = document.querySelector("ul")
+
+console.log(heading)
+console.log(paragraph)
+console.log(listItems)
